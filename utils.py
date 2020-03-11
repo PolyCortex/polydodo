921e5ba3ed3f1eb2a491fec9901f192e66180715
wihar <william.harvey@polymtl.ca>
2020-03-11 19:58:27 -0400

    Add final exploration notebook with outputs

--- utils.py
+++ utils.py
@@ -1,14 +1,40 @@
 import numpy as np
-
+import pandas as pd
+import mne
 from mne.datasets.sleep_physionet._utils import _check_subjects, _fetch_one, _data_path
 
 """
 Code extracted from `https://github.com/mne-tools/mne-python/blob/maint/0.19/mne/datasets/sleep_physionet/age.py`
-to change subject range from 20 to 82, in order to have access to the full dataset.
+to change subject range from 20 to 82, in order to have access to the full dataset. Previously to this customization,
+a magic number was used and limited the api to get only 20 subjects.
 """
 
 BASE_URL = 'https://physionet.org/physiobank/database/sleep-edfx/sleep-cassette/'
 AGE_SLEEP_RECORDS = './data/SC-index.csv'
+
+EVENT_ID = {
+    'Sleep stage W': 0,
+    'Sleep stage 1': 1,
+    'Sleep stage 2': 2,
+    'Sleep stage 3/4': 3,
+    'Sleep stage R': 4,
+    'Sleep stage ?': 5,
+    'Movement time': 6
+}
+
+ANNOTATIONS_EVENT_ID = {
+    'Sleep stage W': 0,
+    'Sleep stage 1': 1,
+    'Sleep stage 2': 2,
+    'Sleep stage 3': 3,
+    'Sleep stage 4': 3,
+    'Sleep stage R': 4,
+    'Sleep stage ?': 5,
+    'Movement time': 6
+}
+
+THIRTY_MINS_IN_SECS = 1800
+EPOCH_DURATION = 30. # in seconds
 
 def fetch_data(subjects, recording=[1, 2], path=None, force_update=False,
                update_path=None, base_url=BASE_URL,
@@ -74,3 +100,46 @@ def fetch_data(subjects, recording=[1, 2], path=None, force_update=False,
                 fnames.append([psg_fname, hyp_fname])
 
     return fnames
+
+
+def get_epochs(files, subject_files, df_subject_information, max_time, cropping=True, equalize_event_counts=False):
+
+    all_subject_epochs = []
+    print("Creating epochs for files : ", files)
+    for i in files:
+        # 1. get all epochs of one person
+        raw_data = mne.io.read_raw_edf(subject_files[i][0], preload=False, verbose=False, exclude=["Temp rectal", "Event marker", "Resp oro-nasal", "EMG submental", "EOG horizontal"])
+        # 2. add the sleep stage annotation to the raw data
+        annotations = mne.read_annotations(subject_files[i][1])
+        raw_data.set_annotations(annotations, emit_warning=False)
+        # 3. We crop everything before lights off and 30 minutes after getting up of bed
+        if cropping:
+            lights_off = df_subject_information.loc[i, 'LightsOffSecond']
+            night_duration = df_subject_information.loc[i, 'NightDuration']
+            woke_up_time = lights_off + night_duration
+            raw_data.crop(tmin=lights_off, tmax=min(woke_up_time + THIRTY_MINS_IN_SECS, raw_data.times[-1]))
+
+        # 4. transform into epochs
+        events, annot_event_id = mne.events_from_annotations(raw_data, event_id=ANNOTATIONS_EVENT_ID.copy(), chunk_duration=EPOCH_DURATION, verbose=False)
+
+        # 5. Few files do not have N3 sleep (i.e. SC4202EC-Hypnogram), so we have to filter out key-value pairs that are not in the annotations.
+        event_ids = {
+            event_key: EVENT_ID[event_key]
+            for event_key in EVENT_ID
+            if EVENT_ID[event_key] in annot_event_id.values()
+        }
+
+        try:
+            epochs = mne.Epochs(raw=raw_data, events=events, event_id=event_ids, tmin=0., tmax=max_time, baseline=None, verbose=False)
+        except ValueError as e:
+            # If no ? epochs are created, we need to ignore this class
+            event_ids.pop('Sleep stage ?', None)
+            epochs = mne.Epochs(raw=raw_data, events=events, event_id=event_ids, tmin=0., tmax=max_time, baseline=None, verbose=False)
+
+        if equalize_event_counts:
+            epochs.equalize_event_counts(event_ids.keys())
+
+        all_subject_epochs.append(epochs)
+
+    print("Concatenating all epochs together...")
+    return mne.concatenate_epochs(all_subject_epochs)
