5077fa60d689661b9764f7253380b7e6871a113e
Samuel Arseneault <samuel.arseneault@polymtl.ca>
2020-04-19 15:46:06 -0400

    Cleanup + axis + tooltip

--- projet_inf8808/Spectrogramme/assets/scripts/1-preproc.js
+++ projet_inf8808/Spectrogramme/assets/scripts/1-preproc.js
@@ -1,47 +1,50 @@
 "use strict";
 
 /**
- * Fichier permettant de traiter les données provenant du fichier CSV.
+ * Fichier permettant de traiter les données provenant du fichier JSON.
  */
 
 /**
  * Précise le domaine en associant un stage de sommeil à une couleur précise.
  *
  * @param color   Échelle de 10 couleurs.
- * @param data    Données provenant du fichier CSV.
+ * @param data    Données provenant du fichier JSON.
  */
 function domainColor(color, data) {
   var maxIntensity = d3.max(data.Fpz_Cz.map(d => d3.max(d)))
   var minIntensity = d3.min(data.Fpz_Cz.map(d => d3.min(d)))
-  console.log(`min:${minIntensity}, max: ${maxIntensity}`)
   color.domain([minIntensity, maxIntensity])
       .interpolator(d3.interpolateRainbow);
 }
 
 /**
- * Précise le domaine des échelles utilisées par les graphiques "focus" et "contexte" pour l'axe X.
+ * Précise le domaine des échelles utilisées par le graphique pour l'axe X.
  *
- * @param xFocus      Échelle en X utilisée avec le graphique "focus".
+ * @param x      Échelle en X utilisée avec le graphique.
  * @param data        Données provenant du fichier JSON.
  */
-function domainX(xFocus, data) {
-  xFocus.domain([0, data.Fpz_Cz.length]);
+function domainX(x, data) {
+  x.domain([0, data.Fpz_Cz.length]);
 }
 
 /**
- * Précise le domaine des échelles utilisées par les graphiques "focus" et "contexte" pour l'axe Y.
+ * Précise le domaine des échelles utilisées par le graphique pour l'axe Y.
  *
- * @param yFocus      Échelle en Y utilisée avec le graphique "focus".
+ * @param y           Échelle en Y utilisée avec le graphique.
+ * @param yAxisScale           Échelle en Y utilisée pour l'axe.
  * @param data        Données provenant du fichier JSON.
  */
-function domainY(yFocus, data) {
-  yFocus.domain(data.Frequencies);
+function domainY(y, yAxisScale,  data) {
+  var sortedFrequencies = data.Frequencies
+  sortedFrequencies.sort((a,b) => b - a)
+  y.domain(sortedFrequencies);
+  yAxisScale.domain([sortedFrequencies[0], sortedFrequencies[sortedFrequencies.length -1]])
 }
 
 /**
  * Trie les données par nom de rue puis par date.
  *
- * @param data      Données provenant du fichier CSV.
+ * @param data      Données provenant du fichier JSON.
  *
  * @return Array    Les données triées qui seront utilisées pour générer les graphiques.
  *                  L'élément retourné doit être un tableau d'objets comptant 10 entrées, une pour chaque rue
@@ -49,11 +52,9 @@ function domainY(yFocus, data) {
  *
  *                  [
  *                    {
- *                      name: string      // Le nom de la rue,
- *                      values: [         // Le tableau compte 365 entrées, pour les 365 jours de l'année.
- *                        date: Date,     // La date du jour.
- *                        count: number   // Le nombre de vélos compté ce jour là (effectuer une conversion avec parseInt)
- *                      ]
+ *                      Frequency: float      // La fréquence du datum
+ *                      Intensity: int        // L'intensity de cette fréquence
+ *                      Timestamp: int        // Le temps du datum (sec)
  *                    },
  *                     ...
  *                  ]
--- projet_inf8808/Spectrogramme/assets/scripts/main.js
+++ projet_inf8808/Spectrogramme/assets/scripts/main.js
@@ -8,41 +8,41 @@
   "use strict";
 
   /***** Configuration *****/
-  // Graphique principal (focus)
-  var marginFocus = {
+  // Graphique principal ()
+  var margin = {
     top: 100,
     right: 10,
     bottom: 100,
     left: 100
   };
-  var widthFocus = 1200 /*- marginFocus.left - marginFocus.right*/;
-  var heightFocus = 600 - marginFocus.top - marginFocus.bottom;
+  var width = 2000 /*- margin.left - margin.right*/;
+  var height = 1200 - margin.top - margin.bottom;
    
   /***** Échelles *****/
-  var xFocus = d3.scaleTime().range([0, widthFocus]);
-  var yFocus = d3.scaleBand()
-  .range([0, heightFocus])
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleBand()
+  .range([0, height])
+  var yAxisScale = d3.scaleLinear()
+  .range([0, height])
 
   /****** Axes *******/
-  var xAxisFocus = d3.axisBottom(xFocus).tickFormat(localization.getFormattedDate);
-  var yAxisFocus= d3.axisLeft().scale(yFocus).ticks(5, "s")
-  .tickSize(-widthFocus);//will create the lines in second visualisation
+  var xAxis = d3.axisBottom(x).tickFormat(getLabelFromIndex);
+  var yAxis= d3.axisLeft(yAxisScale).ticks(5, "s")
 
   /***** Création des éléments *****/
   var svg = d3.select("body")
     .append("svg")
-    .attr("width", widthFocus + marginFocus.left + marginFocus.right)
-    .attr("height", heightFocus + marginFocus.top + marginFocus.bottom);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
 
-  // Groupe affichant le graphique principal (focus).
-  var focus = svg.append("g")
-    .attr("transform", "translate(" + marginFocus.left + "," + marginFocus.top + ")");
+  // Groupe affichant le graphique principal ().
+  var spectrogram = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
   /***** Chargement des données *****/
   d3.json("./data/spectrograms.json").then(function(data){
-    console.log("SAN KALEWWA ASANY MEWAWA")
-    console.log(data);
+
      /***** Prétraitement des données *****/
     var color = d3.scaleSequential();
 
@@ -53,20 +53,27 @@
     domainColor(color, data);
 
     var sources = createSources(data);
-    console.log(sources)
-    domainX(xFocus, data);
-    domainY(yFocus, data);
+    domainX(x, data);
+    domainY(y, yAxisScale, data);
 
     // /***** Création du graphique Stacked bar chart *****/
-    createStackedBarChart(focus, sources, xFocus, yFocus, color, widthFocus, tip, xAxisFocus, yAxisFocus);
+    createSpectrgrammeBarChart(spectrogram, sources, x, y, color, tip, xAxis, yAxis);
    
-    // // Axes focus
-    // focus.append("g")
-    //   .attr("class", "x axis")
-    //   .attr("transform", "translate(0," + (heightFocus - 200) + ")")
-    //   .call(xAxisFocus)
-    //   .selectAll("text")
-    //   .style("font-size", "18px");
+    // Axes 
+    spectrogram.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("font-size", "18px");
+
+    spectrogram.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(0" + ",0)")
+      .call(yAxis)
+      .selectAll("text")
+      .style("font-size", "18px");
+
 
     // //get tick
     // d3.selectAll(".tick")
@@ -74,15 +81,10 @@
     //   .style("font-weight", function(d,i) {return 540})
       
     // /***** Création de l'infobulle *****/
-    // tip.html(function(d) {
-    //   return getToolTipText.call(this, d);
-    // });
-    // svg.call(tip);
-   
-    // tipStacked.html(function(d) {
-    //   return getStackedToolTipText.call(this, d,totalStagesPortion,totalTimeStamp);
-    // });
-    // svg.call(tipStacked);
+    tip.html(function(d) {
+      return getToolTipText.call(this, d);
+    });
+    svg.call(tip);
 
     // /***** Création de la légende *****/
     // legend(svg, states, color);
