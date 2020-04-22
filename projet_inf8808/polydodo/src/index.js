73d4395e5e8b1131bc867d9936c60ab8ae9b9cda
William Harvey <william.harvey@polymtl.ca>
2020-04-22 01:01:47 -0400

    Add some content to analysis page

--- projet_inf8808/polydodo/src/index.js
+++ projet_inf8808/polydodo/src/index.js
@@ -6,7 +6,8 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
-import SleepAnalysis from "views/SleepAnalysis/sleep_analysis";
+import SleepAnalysis from "views/sleep_analysis/sleep_analysis";
+import Performance from "views/performance/performance";
 import Emoji from "components/emoji";
 
 const UnderConstruction = <h1 className="">Under Construction... <Emoji symbol="🏗️"/></h1>
@@ -17,7 +18,7 @@ ReactDOM.render(
       <Route exact path="/" render={props => <SleepAnalysis/>} />
       <Route exact path="/record-my-sleep" render={props => UnderConstruction } />
       <Route exact path="/analyze-my-sleep" render={props => <SleepAnalysis/> } />
-      <Route exact path="/performance" render={props => UnderConstruction } />
+      <Route exact path="/performance" render={props => <Performance/> } />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
---
+++ projet_inf8808/polydodo/src/views/sleep_analysis/sleep_analysis.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import {Container, Row} from "reactstrap";
+
+import Navbar from "../../components/navbar/navbar";
+import Header from "../../components/header";
+import Footer from "../../components/footer/footer";
+
+import Placeholder from "../../assets/img/placeholder.png";
+
+import text from "./text.json";
+
+const SleepAnalysis = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header
+        sizeClass={"pb-150"}
+        shapeQty={7}
+        title={text["header_title"]}
+        subtitle={text["header_subtitle"]}
+        description={text["header_description"]}
+      />
+      <Container className="mt-5 text-justify">
+        <p>
+          Of course, we are analyzing only one night of sleep so it is therefore tricky to draw general conclusions about your sleep. It is however fascinating to see how your night was.
+        </p>
+        <p>
+          Without further ado, this is what was your night of sleep:
+        </p>
+        <Row className="justify-content-center">
+          <img className="mt-7 mb-7"  width="50%" alt="viz placeholder" src={Placeholder}/>
+        </Row>
+        <p>
+          We have seen that sleep can be decomposed in mainly two stages, whereas REM and NREM, and that we can observe different stage proportions across age, gender and different sleep disorders. We’ve also defined other measures of your sleep architecture, such as your sleep latency, efficiency and total sleep time. In order to improve your sleep hygiene, many elements can be considered:
+        </p>
+        <ul>
+          <li>
+            Alimentation: having a balanced diet and avoiding sources of caffeine can have a positive impact on one’s sleep. Chocolate, soft drink, tea and decaffeinated coffee are unexpected sources of caffeine.
+          </li>
+          <li>
+            Routine: going to sleep about at the same time, in a darkened and quiet environment. 
+          </li>
+        </ul>
+        <p>
+          Although we’ve looked at many aspects of your night’s sleep, we haven’t properly looked at your sleep dynamics, whereas how your sleep evolves overnight.
+        </p>
+        <Row className="justify-content-center">
+          <img className="mt-7 mb-7"  width="50%" alt="viz placeholder" src={Placeholder}/>
+        </Row>
+        <p>
+          Sleep cycles take place in a broader process, named the circadian rhythm. It is the one that regulates our wake and sleep cycles over a 24 hours period. 
+        </p>
+        <p>
+          You’ve been able to visualize and inspect your night of sleep, which we’ve classified only based on your EEG recordings. In a sleep lab, electrophysiology technicians generally look at your EEG, EOG and submental EMG, and then manually classify each epoch of 30 seconds that compose your night. By looking at your EEG recordings, we can see some patterns that can help electrophysiology technicians, and our classifier, discriminate sleep stages throughout the night.
+        </p>
+        <Row className="justify-content-center">
+          <img className="mt-7 mb-7"  width="50%" alt="viz placeholder" src={Placeholder}/>
+        </Row>
+      </Container >
+      <Footer />
+    </div>
+  );
+}
+
+export default SleepAnalysis;
