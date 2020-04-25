d684c65423b08429cc5d1badf0ced53f058547b4
William Harvey <william.harvey@polymtl.ca>
2020-04-25 16:49:33 -0400

    Scroll To top when routing from to another component

--- polydodo/src/index.js
+++ polydodo/src/index.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter,
   Route,
   Switch,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
@@ -14,19 +14,20 @@ import "assets/css/visualisation.css";
 
 import SleepAnalysis from "views/sleep_analysis/sleep_analysis";
 import Performance from "views/performance/performance";
+import ScrollToTop from "components/scroll_to_top";
 import Emoji from "components/emoji";
-
-const UnderConstruction = <h1 className="">Under Construction... <Emoji symbol="🏗️"/></h1>
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" render={props => <SleepAnalysis/>} />
-      <Route exact path="/record-my-sleep" render={props => UnderConstruction } />
-      <Route exact path="/analyze-my-sleep" render={props => <SleepAnalysis/> } />
-      <Route exact path="/performance" render={props => <Performance/> } />
-      <Redirect to='/' />
-    </Switch>
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" render={() => <SleepAnalysis />} />
+        <Route exact path="/record-my-sleep" render={() => <h1 className="">Under Construction... <Emoji symbol="🏗️" /></h1>} />
+        <Route exact path="/analyze-my-sleep" render={() => <SleepAnalysis />} />
+        <Route exact path="/performance" render={() => <Performance />} />
+        <Redirect to='/' />
+      </Switch>
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById("root")
 );
