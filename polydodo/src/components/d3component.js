d75d2a768f6c8fc29f673a9ef2663a836bcf8e39
William Harvey <william.harvey@polymtl.ca>
2020-06-21 20:49:27 -0400

    Extract csv read from d3 to a react custom hook

--- polydodo/src/components/d3component.js
+++ polydodo/src/components/d3component.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-const D3Component = React.memo(({ callback }) => {
+const D3Component = React.memo(({ callback, data }) => {
   const ref = useRef();
-  useEffect(() => callback(ref.current));
+  useEffect(() => {
+    callback(ref.current, data);
+  });
   return <svg ref={ref} />;
 });
+
+D3Component.propTypes = {
+  callback: PropTypes.func.isRequired,
+  data: PropTypes.any.isRequired,
+};
 
 export default D3Component;
