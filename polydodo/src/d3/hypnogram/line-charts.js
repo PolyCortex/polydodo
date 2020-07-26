f1aa9733bf5f3e451a873e0f8611572e48434222
William Harvey <william.harvey@polymtl.ca>
2020-07-26 16:26:34 -0400

    export less functions so d3 code is clearer

--- polydodo/src/d3/hypnogram/line-charts.js
+++ polydodo/src/d3/hypnogram/line-charts.js
@@ -1,30 +1,48 @@
 import * as d3 from "d3";
 import _ from "lodash";
 
-export const createLine = (x, y) => {
-  return d3.line()
-    .x(d => x(d.timestamp))
-    .y(d => y(d.sleep_stage))
-    .curve(d3.curveStepAfter);
-};
+import createMouseOver from "./mouse-over";
+import { DIMENSIONS, MARGINS } from "./constants";
 
-export const createHypnogramChart = (g, data, line, color) => {
-  const g_chart = g.append("g")
-    .attr("class", "hypnogram-lines");
-
-  g_chart.selectAll()
+const createHypnogramChart = (
+  g,
+  data,
+  x,
+  y,
+  xAxis,
+  yAxis,
+  color,
+  chartTitle,
+  hypnogramNames,
+  comparativeColors
+) => {
+  const line = createLine(x, y);
+  const g_chart = g.append("g").attr("class", "hypnogram-lines");
+  g_chart
+    .selectAll()
     .data(data)
-    .enter().append("path")
+    .enter()
+    .append("path")
     .attr("class", "line")
     .attr("fill", "none")
-    .attr("d", x => line(x.values))
-    .attr("stroke", x => color(x.name))
+    .attr("d", (x) => line(x.values))
+    .attr("stroke", (x) => color(x.name))
     .attr("stroke-width", 2);
 
-  return g_chart;
+  createMouseOver(g_chart, x, y, data, MARGINS, DIMENSIONS, color);
+  createAxes(g, xAxis, yAxis, DIMENSIONS, MARGINS);
+  createTitle(g, chartTitle, DIMENSIONS, MARGINS);
+  createLegend(g, hypnogramNames, comparativeColors, MARGINS);
 };
 
-export const createAxes = (g, xAxis, yAxis, dimensions, margin) => {
+const createLine = (x, y) =>
+  d3
+    .line()
+    .x((d) => x(d.timestamp))
+    .y((d) => y(d.sleep_stage))
+    .curve(d3.curveStepAfter);
+
+const createAxes = (g, xAxis, yAxis, dimensions, margin) => {
   const { height, width } = dimensions;
 
   g.append("g")
@@ -32,57 +50,62 @@ export const createAxes = (g, xAxis, yAxis, dimensions, margin) => {
     .attr("transform", `translate(0,${height})`)
     .call(xAxis);
 
-  g.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
+  g.append("g").attr("class", "y axis").call(yAxis);
 
-  g.append("text").text("Time")
+  g.append("text")
+    .text("Time")
     .attr("text-anchor", "end")
     .attr("x", width)
-    .attr("y", height + (1/2)*margin.bottom);
+    .attr("y", height + (2 / 3) * margin.bottom);
 
-  g.append("text").text("Sleep stage")
+  g.append("text")
+    .text("Sleep stage")
     .attr("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
-    .attr("x", -height/2)
-    .attr("y", -(1/2)*margin.left);
+    .attr("x", -height / 2)
+    .attr("y", -(2 / 3) * margin.left);
 };
 
-export const createTitle = (g, title, dimensions, margin) => {
-  g.append("text").text(title)
+const createTitle = (g, title, dimensions, margin) => {
+  g.append("text")
+    .text(title)
     .attr("class", "chart-title")
     .attr("text-anchor", "middle")
-    .attr("y", -(3/4)*margin.top)
-    .attr("x", (1/2)*dimensions.width);
+    .attr("y", -(3 / 4) * margin.top)
+    .attr("x", (1 / 2) * dimensions.width);
 };
 
-export const createLegend = (g, hypnogramNames, comparativeColors, dimensions, margin) => {
-  const legendData = _.zip(hypnogramNames, comparativeColors).map(x => {
+const createLegend = (g, hypnogramNames, comparativeColors, margin) => {
+  const legendData = _.zip(hypnogramNames, comparativeColors).map((x) => {
     return {
-      "name": x[0],
-      "color": x[1],
+      name: x[0],
+      color: x[1],
     };
   });
 
   g.selectAll(".rect.legend")
     .data(legendData)
-  .enter().append("rect")
+    .enter()
+    .append("rect")
     .attr("class", "legend")
     .attr("stroke-width", 1)
-    .attr("fill", x => x.color)
+    .attr("fill", (x) => x.color)
     .attr("width", "1em")
     .attr("height", "1em")
-    .attr("y", -(1/2)*margin.top)
+    .attr("y", -(1 / 2) * margin.top)
     .attr("x", (_, i) => `${i * 8}em`);
 
   g.selectAll(".text.legend")
     .data(legendData)
-  .enter().append("text")
+    .enter()
+    .append("text")
     .attr("class", "legend")
-    .text(x => x.name)
+    .text((x) => x.name)
     .attr("font-size", 12)
     .attr("dominant-baseline", "hanging")
-    .attr("y", -(1/2)*margin.top)
-    .attr("dy", 0.25+"em")
+    .attr("y", -(1 / 2) * margin.top)
+    .attr("dy", 0.25 + "em")
     .attr("x", (_, i) => `${1.5 + i * 11}em`);
 };
+
+export default createHypnogramChart;
--- polydodo/src/d3/hypnogram/preproc.js
+++ polydodo/src/d3/hypnogram/preproc.js
@@ -1,4 +1,3 @@
-import * as d3 from "d3";
 import _ from "lodash";
 
 const RAW_DATA_LABELS = {
@@ -9,7 +8,17 @@ const RAW_DATA_LABELS = {
   REM: 4,
 };
 
-const SLEEP_LABELS = ["W", "REM", "N1", "N2", "N3"];
+export const preprocessData = (data, hypnogramNames) => {
+  data = convertTimestampsToDates(data);
+  data = convertValuesToLabels(data);
+
+  return _.zip(data, hypnogramNames).map((x) =>
+    Object({
+      name: x[1],
+      values: x[0],
+    })
+  );
+};
 
 const parseTimestampToDate = (timestamp) => {
   // To convert UNIX timestamp to JS Date, we have to convert number of seconds to milliseconds.
@@ -24,14 +33,14 @@ const parseTimestampToDate = (timestamp) => {
   );
 };
 
-export const convertTimestampsToDates = (data) =>
+const convertTimestampsToDates = (data) =>
   data.map((hypnogram) =>
     hypnogram.map((row) =>
       Object({ ...row, timestamp: parseTimestampToDate(row.timestamp) })
     )
   );
 
-export const convertValuesToLabels = (data) =>
+const convertValuesToLabels = (data) =>
   data.map((hypno) =>
     hypno.map((row) =>
       Object({
@@ -42,30 +51,3 @@ export const convertValuesToLabels = (data) =>
       })
     )
   );
-
-export const convertSources = (data, hypnogramNames) =>
-  _.zip(data, hypnogramNames).map((x) =>
-    Object({
-      name: x[1],
-      values: x[0],
-    })
-  );
-
-export const domainX = (x, data) => {
-  // TODO: Préciser les domaines pour les variables "xFocus" et "xContext" pour l'axe X.
-  const dates = data[0].values.map((datum) => datum.timestamp);
-  x.domain([d3.min(dates), d3.max(dates)]);
-};
-
-export const domainY = (y) => {
-  y.domain(SLEEP_LABELS);
-};
-
-export const domainColor = (data, comparativeColors) => {
-  const numberHypnogram = data.length;
-  const colors = comparativeColors.slice(0, numberHypnogram);
-  return d3
-    .scaleOrdinal()
-    .domain(data.map((x) => x.name))
-    .range(colors);
-};
