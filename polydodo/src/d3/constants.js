98cd03f64cbc735bba185af6bfa46d1d39262758
William Harvey <william.harvey@polymtl.ca>
2020-07-26 22:18:25 -0400

    changed csv file sleep stages for labels instead of integers

--- polydodo/src/d3/constants.js
+++ polydodo/src/d3/constants.js
@@ -1,11 +1,11 @@
 export const TRANSITION_TIME_MS = 1000;
 
 export const STAGES = ["W", "N1", "N2", "N3", "REM"];
-export const STATE_TO_COLOR = {
-  W: '#E3624B',
-  N1: '#B0C9D9',
-  N2: '#4da6fe',
-  N3: '#48587f',
-  REM: '#FFD443',
+export const STAGE_TO_COLOR = {
+  W: "#E3624B",
+  N1: "#B0C9D9",
+  N2: "#4da6fe",
+  N3: "#48587f",
+  REM: "#FFD443",
 };
 export const STAGES_ORDERED = ["W", "REM", "N1", "N2", "N3"];
