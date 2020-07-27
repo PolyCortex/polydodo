a18a50b3e0bf79a4b8b23b8531542ffce756dee1
William Harvey <william.harvey@polymtl.ca>
2020-07-26 22:18:25 -0400

    changed csv file sleep stages for labels instead of integers

--- polydodo/src/d3/constants.js
+++ polydodo/src/d3/constants.js
@@ -1,7 +1,7 @@
 export const TRANSITION_TIME_MS = 1000;
 
 export const STAGES = ["W", "N1", "N2", "N3", "REM"];
-export const STATE_TO_COLOR = {
+export const STAGE_TO_COLOR = {
   W: "#E3624B",
   N1: "#B0C9D9",
   N2: "#4da6fe",
--- polydodo/src/d3/hypnogram/preproc.js
+++ polydodo/src/d3/hypnogram/preproc.js
@@ -2,17 +2,8 @@ import _ from "lodash";
 
 import { convertTimestampsToDates } from "../utils";
 
-const RAW_DATA_LABELS = {
-  W: 0,
-  N1: 1,
-  N2: 2,
-  N3: 3,
-  REM: 4,
-};
-
 export const preprocessData = (data, hypnogramNames) => {
   data = data.map((hypno) => convertTimestampsToDates(hypno));
-  data = convertValuesToLabels(data);
 
   return _.zip(data, hypnogramNames).map((x) =>
     Object({
@@ -21,15 +12,3 @@ export const preprocessData = (data, hypnogramNames) => {
     })
   );
 };
-
-const convertValuesToLabels = (data) =>
-  data.map((hypno) =>
-    hypno.map((row) =>
-      Object({
-        ...row,
-        sleepStage: Object.keys(RAW_DATA_LABELS).find(
-          (key) => RAW_DATA_LABELS[key] === parseInt(row.sleepStage)
-        ),
-      })
-    )
-  );
