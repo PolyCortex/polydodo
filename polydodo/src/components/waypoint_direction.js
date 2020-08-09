90fca798bd7a4b7f04cfee7cb40bca8efe22f413
William Harvey <william.harvey@polymtl.ca>
2020-08-09 13:24:02 -0400

    changed callbacks to considerate from which state and to which state and only coded differences

--- polydodo/src/components/waypoint_direction.js
+++ polydodo/src/components/waypoint_direction.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Waypoint } from "react-waypoint";
 
-const WaypointDirection = ({ onUp, onDown }) => (
+const WaypointDirection = ({ onUp = () => {}, onDown = () => {} }) => (
   <Waypoint
     onEnter={({ previousPosition }) =>
       previousPosition === Waypoint.below ? onDown() : onUp()
