4548de6c5065324b062a6b571276b093d3ab9910
William Harvey <william.harvey@polymtl.ca>
2020-08-16 22:44:42 -0400

    used canvas to display rectangles and removed bins

--- polydodo/src/d3/spectrogram/constants.js
+++ polydodo/src/d3/spectrogram/constants.js
@@ -1,25 +1,36 @@
+import _ from "lodash";
+
 const SECONDS_PER_DATUM = 30;
-export const DATUM_PER_TIMESTAMP = 4;
+const PADDING = 50;
+export const FREQUENCY_BINS = 1;
+export const DATUM_PER_TIMESTAMP = 1;
 export const TIMESTAMP_DURATION = SECONDS_PER_DATUM * DATUM_PER_TIMESTAMP;
-export const FREQUENCY_BINS = 5;
 export const NB_SPECTROGRAM = 2;
 export const FREQUENCY_KEY = "frequencies";
 
 export const MARGIN = {
-  TOP: 100,
+  TOP: 50,
   RIGHT: 200,
   BOTTOM: 50,
   LEFT: 70,
 };
 export const CANVAS_DIMENSION = {
   WIDTH: 1000,
-  HEIGHT: 1200,
+  HEIGHT: 1000,
 };
 export const DIMENSION = {
   WIDTH: CANVAS_DIMENSION.WIDTH - MARGIN.LEFT - MARGIN.RIGHT,
   HEIGHT: CANVAS_DIMENSION.HEIGHT - MARGIN.BOTTOM - MARGIN.TOP,
 };
-export const SPECTROGRAM_CANVAS_HEIGTH =
-  CANVAS_DIMENSION.HEIGHT / NB_SPECTROGRAM;
+export const SPECTROGRAM_CANVAS_HEIGTH = _.range(NB_SPECTROGRAM).map((x) => {
+  let height = DIMENSION.HEIGHT / NB_SPECTROGRAM;
+  if (x === 0) {
+    height += MARGIN.TOP;
+  } else if (x === NB_SPECTROGRAM - 1) {
+    height += MARGIN.BOTTOM;
+  }
+  return height;
+});
 export const SPECTROGRAM_HEIGHT =
-  SPECTROGRAM_CANVAS_HEIGTH - (MARGIN.BOTTOM + MARGIN.TOP) / NB_SPECTROGRAM;
+  (CANVAS_DIMENSION.HEIGHT - MARGIN.BOTTOM - MARGIN.TOP - PADDING) /
+  NB_SPECTROGRAM;
