fd4fa58b8eb376244c1aa43e00e859d427075322
abelfodil <abelfodil@users.noreply.github.com>
2020-09-17 14:42:12 +0000

    Deploying to gh-pages from  @ ae2b5ea179c19d4c14782c01f09040433bdffee4 🚀

---
+++ precache-manifest.0620cc9d210e2f5d8b266b338a7186e9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f261fececc8044c1f2ffa33656367a28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae51e798ae72d2f11657",
+    "url": "./static/css/2.20b031e0.chunk.css"
+  },
+  {
+    "revision": "1c49f7508764c23634fd",
+    "url": "./static/css/main.3c7ce497.chunk.css"
+  },
+  {
+    "revision": "ae51e798ae72d2f11657",
+    "url": "./static/js/2.4efdc28d.chunk.js"
+  },
+  {
+    "revision": "9aae5077729947dfa90ccff7014f56af",
+    "url": "./static/js/2.4efdc28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c49f7508764c23634fd",
+    "url": "./static/js/main.a03a12c8.chunk.js"
+  },
+  {
+    "revision": "123de55f7ada792f1f3b",
+    "url": "./static/js/runtime-main.5027c178.js"
+  },
+  {
+    "revision": "fda414f11bc60efd4c9a12c1dcdecd97",
+    "url": "./static/media/aep.fda414f1.png"
+  },
+  {
+    "revision": "e3ba6abc64ead779a4210d241889bd11",
+    "url": "./static/media/diptrace.e3ba6abc.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9ec7437f38e2b89dd4e4a16ad13b3f5a",
+    "url": "./static/media/hypnogram-electrophysiologist.9ec7437f.csv"
+  },
+  {
+    "revision": "f61cd2b4596ca0af50c1306834c455ab",
+    "url": "./static/media/hypnogram-openbci-electrophysiologist.f61cd2b4.csv"
+  },
+  {
+    "revision": "c2f4d4bd0f377dba57a987d15f27a60a",
+    "url": "./static/media/hypnogram-openbci-predicted.c2f4d4bd.csv"
+  },
+  {
+    "revision": "426e51a07e4036fb20352af91725003f",
+    "url": "./static/media/hypnogram-predicted.426e51a0.csv"
+  },
+  {
+    "revision": "391fef8a9ba02302a9d3613ac080c490",
+    "url": "./static/media/hypnogram.391fef8a.csv"
+  },
+  {
+    "revision": "bcea7572194d7b7245345be1e616b877",
+    "url": "./static/media/logo.bcea7572.png"
+  },
+  {
+    "revision": "bdd09ab5b6a9db96973a5dec7b18b258",
+    "url": "./static/media/ntx.bdd09ab5.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "./static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "./static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "./static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "./static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "./static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "3034d9919e43a4a2ab115b5ade62ab02",
+    "url": "./static/media/polymtl.3034d991.png"
+  },
+  {
+    "revision": "53bfde7ac5b2c91c549fd72425eb7957",
+    "url": "./static/media/protocase.53bfde7a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f1893bdd5fa3770d02d7a24e5df5a98.js"
+  "./precache-manifest.0620cc9d210e2f5d8b266b338a7186e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
