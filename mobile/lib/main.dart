37b8cc24139e8afccc525939dcf8f774f7308ba2
MouradLachhab <mourad.lachhab@yahoo.com>
2020-09-20 19:36:23 -0400

    Requete de donnees et enregistrement sur csv

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:binary/binary.dart';
+import 'package:csv/csv.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -54,16 +58,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   static String BLE_SERVICE = "fe84";
   static String BLE_RECEIVE = "2d30c082";
   static String BLE_SEND = "2d30c083";
   static String BLE_DISCONNECT = "2d30c084";
 
-
   BluetoothDevice _connectedDevice;
   List<BluetoothService> _services;
-  List<BluetoothCharacteristic> _characteristics;
+  BluetoothCharacteristic _sendCharacteristics;
+  BluetoothCharacteristic _receiveCharacteristics;
+  List<List> _values = new List();
+  int sampleCounter = 0;
 
   void addDeviceTolist(final BluetoothDevice device) {
     if (!widget.devicesList.contains(device)) {
@@ -87,6 +92,92 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _connectedDevice = device;
     });
+  }
+
+  List formatData(event) {
+    List data = new List(10);
+
+    data[0] = sampleCounter++;
+    data[1] = (event[1] << 11) | (event[2] << 3) | (event[3] >> 5);
+    data[2] = ((event[3] & 31) << 14) | (event[4] << 6) | (event[5] >> 2);
+    data[3] = ((event[5] & 3) << 17) |
+        (event[6] << 9) |
+        (event[7] << 1) |
+        (event[8] >> 7);
+    data[4] = ((event[8] & 127) << 12) | (event[9] << 4) | (event[10] >> 4);
+    data[5] = sampleCounter++;
+    data[6] = ((event[10] & 15) << 15) | (event[11] << 7) | (event[12] >> 1);
+    data[7] = ((event[12] & 1) << 18) |
+        (event[13] << 10) |
+        (event[14] << 2) |
+        (event[15] >> 6);
+    data[8] = ((event[15] & 63) << 13) | (event[16] << 5) | (event[17] >> 3);
+    data[9] = ((event[17] & 7) << 16) | (event[18] << 8) | (event[19]);
+
+    return data;
+  }
+
+  List handleNegativesAndConvertToVolts(data) {
+    for (int i = 1; i < data.length; ++i) {
+      if (i == 5) continue;
+
+      String binary = data[i].toRadixString(2);
+
+      if (binary[binary.length - 1] == '1') {
+        data[i] = (~data[i] & 524287 | 1) * -1;
+      }
+
+      // Convert to volts using the scale factor
+      data[i] = data[i].toDouble() * 1.2 * 8388607.0 * 1.5 * 51.0;
+    }
+
+    return data;
+  }
+
+  void addData(event) async {
+    int packetID = event[0];
+
+    if (packetID >= 101 && packetID <= 200) {
+      List data = formatData(event);
+      data = handleNegativesAndConvertToVolts(data);
+
+      _values.add(data.sublist(0, 5));
+      _values.add(data.sublist(5, 10));
+    }
+  }
+
+  void updateData(event) async {
+    if (_values.length > 300) {
+      final directory = await getExternalStorageDirectory();
+      final pathOfTheFileToWrite = directory.path + "/myCsvFile.csv";
+      File file = File(pathOfTheFileToWrite);
+      String csv = const ListToCsvConverter().convert(_values);
+      file.writeAsString(csv);
+
+      String s = 's';
+      print("stopping");
+      print(pathOfTheFileToWrite);
+      print("____________________________________________________");
+      await _sendCharacteristics.write(s.codeUnits);
+    }
+
+    setState(() => {addData(event)});
+  }
+
+  void startDataStream(List<BluetoothCharacteristic> c) async {
+    for (BluetoothCharacteristic char in c) {
+      if (char.toString().contains(BLE_RECEIVE)) {
+        _receiveCharacteristics = char;
+        await char.setNotifyValue(true);
+        char.value.listen((event) {
+          updateData(event);
+        });
+      } else if (char.toString().contains(BLE_SEND)) {
+        _sendCharacteristics = char;
+        String b = 'b';
+        await _sendCharacteristics.write(b.codeUnits);
+      }
+    }
   }
 
   @override
@@ -126,26 +217,29 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (context, index) {
           return Card(
               child: ListTile(
-                onTap: () => setState(() => {_characteristics = _services[index].characteristics}),
-                title: Text(_services[index].uuid.toString().contains("fe84") ? "YES" : "NO"),
+            onTap: () => setState(
+                () => startDataStream(_services[index].characteristics)),
+            title: Text(_services[index].uuid.toString().contains(BLE_SERVICE)
+                ? BLE_SERVICE
+                : "NO"),
           ));
         });
   }
 
   ListView buildListViewOfCharacteristics() {
     return ListView.builder(
-        itemCount: _characteristics.length,
+        itemCount: _values.length,
         itemBuilder: (context, index) {
           return Card(
               child: ListTile(
-                title: Text(_characteristics[index].uuid.toString().contains("2d30c082") ? "YES" : "NO"),
+            title: Text(_values[index].toString()),
           ));
         });
   }
 
   ListView _buildView() {
     if (_connectedDevice != null) {
-      if (_characteristics != null)
+      if (_values.isNotEmpty)
         return buildListViewOfCharacteristics();
       else
         return buildListViewOfConnectedDevice();
