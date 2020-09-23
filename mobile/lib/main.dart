a7565324199456051dad443603b75391b8a4fbc1
MouradLachhab <mourad.lachhab@yahoo.com>
2020-09-23 15:23:59 -0400

    Changement du format du fichier output pour openBCI

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -64,7 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
   static String BLE_DISCONNECT = "2d30c084";
 
   BluetoothDevice _connectedDevice;
-  List<BluetoothService> _services;
   BluetoothCharacteristic _sendCharacteristics;
   BluetoothCharacteristic _receiveCharacteristics;
   List<List> _values = new List();
@@ -87,17 +86,47 @@ class _MyHomePageState extends State<MyHomePage> {
         throw e;
       }
     } finally {
-      _services = await device.discoverServices();
+      List<BluetoothService> services = await device.discoverServices();
+
+      for (BluetoothService service in services) {
+        if (service.toString().contains(BLE_SERVICE)) {
+          for (BluetoothCharacteristic characteristic
+              in service.characteristics) {
+            if (characteristic.toString().contains(BLE_RECEIVE)) {
+              _receiveCharacteristics = characteristic;
+            } else if (characteristic.toString().contains(BLE_SEND)) {
+              _sendCharacteristics = characteristic;
+            }
+          }
+        }
+      }
     }
+
     setState(() {
       _connectedDevice = device;
     });
   }
 
-  List formatData(event) {
-    List data = new List(10);
+  List getListForCSV() {
+    List data = new List(30);
 
-    data[0] = sampleCounter++;
+    for (int i = 5; i < 15; ++i) {
+      data[i] = 0;
+      data[i + 15] = 0;
+    }
+    return data;
+  }
+
+  List formatData(event) {
+    // Test event, comment scale factor
+    // event = [ 101, 0, 0, 0, 0, 8, 0, 5, 0, 0, 72, 0, 9, 240, 1, 176, 0, 48, 0, 8]; // Positive Test
+    // Expected [[0, 2, 10, 4], [262148, 507910, 393222, 8]]
+    // event = [ 101, 255, 255, 191, 255, 239, 255, 252, 255, 255, 88, 0, 11, 62, 56, 224, 0, 63, 240, 1 ]; // Negative Test
+    // Expected [[-3, -5, -7, -11], [-262139, -198429, -262137, -4095]]
+
+    List data = getListForCSV();
+
+    data[0] = sampleCounter;
     data[1] = (event[1] << 11) | (event[2] << 3) | (event[3] >> 5);
     data[2] = ((event[3] & 31) << 14) | (event[4] << 6) | (event[5] >> 2);
     data[3] = ((event[5] & 3) << 17) |
@@ -105,30 +134,32 @@ class _MyHomePageState extends State<MyHomePage> {
         (event[7] << 1) |
         (event[8] >> 7);
     data[4] = ((event[8] & 127) << 12) | (event[9] << 4) | (event[10] >> 4);
-    data[5] = sampleCounter++;
-    data[6] = ((event[10] & 15) << 15) | (event[11] << 7) | (event[12] >> 1);
-    data[7] = ((event[12] & 1) << 18) |
+    data[15] = sampleCounter++;
+    data[16] = ((event[10] & 15) << 15) | (event[11] << 7) | (event[12] >> 1);
+    data[17] = ((event[12] & 1) << 18) |
         (event[13] << 10) |
         (event[14] << 2) |
         (event[15] >> 6);
-    data[8] = ((event[15] & 63) << 13) | (event[16] << 5) | (event[17] >> 3);
-    data[9] = ((event[17] & 7) << 16) | (event[18] << 8) | (event[19]);
+    data[18] = ((event[15] & 63) << 13) | (event[16] << 5) | (event[17] >> 3);
+    data[19] = ((event[17] & 7) << 16) | (event[18] << 8) | (event[19]);
 
     return data;
   }
 
   List handleNegativesAndConvertToVolts(data) {
-    for (int i = 1; i < data.length; ++i) {
-      if (i == 5) continue;
+    for (int i = 1; i < 5; ++i) {
+      for (int j = 0; j < 2; ++j) {
+        int offset = 15 * j;
+        String binary = data[i + offset].toRadixString(2);
 
-      String binary = data[i].toRadixString(2);
+        if (binary[binary.length - 1] == '1') {
+          data[i + offset] = (~data[i + offset] & 524287 | 1) * -1;
+        }
 
-      if (binary[binary.length - 1] == '1') {
-        data[i] = (~data[i] & 524287 | 1) * -1;
+        // Convert to microvolts using the scale factor
+        data[i + offset] =
+            data[i + offset].toDouble() * (1200000 / (8388607.0 * 1.5 * 51.0));
       }
-
-      // Convert to volts using the scale factor
-      data[i] = data[i].toDouble() * 1.2 * 8388607.0 * 1.5 * 51.0;
     }
 
     return data;
@@ -141,43 +172,65 @@ class _MyHomePageState extends State<MyHomePage> {
       List data = formatData(event);
       data = handleNegativesAndConvertToVolts(data);
 
-      _values.add(data.sublist(0, 5));
-      _values.add(data.sublist(5, 10));
+      _values.add(data.sublist(0, 15));
+      _values.add(data.sublist(15, 30));
     }
   }
 
   void updateData(event) async {
-    if (_values.length > 300) {
-      final directory = await getExternalStorageDirectory();
-      final pathOfTheFileToWrite = directory.path + "/myCsvFile.csv";
-      File file = File(pathOfTheFileToWrite);
-      String csv = const ListToCsvConverter().convert(_values);
-      file.writeAsString(csv);
-
-      String s = 's';
-      print("stopping");
-      print(pathOfTheFileToWrite);
-      print("____________________________________________________");
-      await _sendCharacteristics.write(s.codeUnits);
-    }
-
     setState(() => {addData(event)});
   }
 
-  void startDataStream(List<BluetoothCharacteristic> c) async {
-    for (BluetoothCharacteristic char in c) {
-      if (char.toString().contains(BLE_RECEIVE)) {
-        _receiveCharacteristics = char;
-        await char.setNotifyValue(true);
-        char.value.listen((event) {
-          updateData(event);
-        });
-      } else if (char.toString().contains(BLE_SEND)) {
-        _sendCharacteristics = char;
-        String b = 'b';
-        await _sendCharacteristics.write(b.codeUnits);
-      }
-    }
+  void initializeData() {
+    _values.add(["%OpenBCI Raw EEG Data"]);
+    _values.add(["%Number of channels = 4"]);
+    _values.add(["%Sample Rate = 200 Hz"]);
+    _values.add(["%Board = OpenBCI_GUI\$BoardGanglionBLE"]);
+    _values.add([
+      "Sample Index",
+      " EXG Channel 0",
+      " EXG Channel 1",
+      " EXG Channel 2",
+      " EXG Channel 3",
+      " Accel Channel 0",
+      " Accel Channel 1",
+      " Accel Channel 2",
+      " Other",
+      " Other",
+      " Other",
+      " Other",
+      " Other",
+      " Timestamp",
+      " Timestamp (Formatted)"
+    ]);
+  }
+
+  void startDataStream() async {
+    initializeData();
+
+    await _receiveCharacteristics.setNotifyValue(true);
+    _receiveCharacteristics.value.listen((event) {
+      updateData(event);
+    });
+
+    String b = 'b';
+    await _sendCharacteristics.write(b.codeUnits);
+  }
+
+  void stopDataStream() async {
+    String s = 's';
+    await _receiveCharacteristics.setNotifyValue(false);
+    await _sendCharacteristics.write(s.codeUnits);
+    await _connectedDevice.disconnect();
+    _connectedDevice = null;
+
+    final directory = await getExternalStorageDirectory();
+    final pathOfTheFileToWrite = directory.path + "/myCsvFile.txt";
+    File file = File(pathOfTheFileToWrite);
+    String csv = const ListToCsvConverter().convert(_values);
+    await file.writeAsString(csv);
+
+    _values.clear();
   }
 
   @override
@@ -198,51 +251,51 @@ class _MyHomePageState extends State<MyHomePage> {
     widget.flutterBlue.startScan();
   }
 
-  ListView buildListViewOfDevices() {
-    return ListView.builder(
-        itemCount: widget.devicesList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-                onTap: () => connectDevice(widget.devicesList[index]),
-                title: Text(widget.devicesList[index].name),
-                subtitle: Text(widget.devicesList[index].id.toString())),
-          );
-        });
-  }
-
-  ListView buildListViewOfConnectedDevice() {
-    return ListView.builder(
-        itemCount: _services.length,
-        itemBuilder: (context, index) {
-          return Card(
+  Scaffold buildListViewOfDevices() {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: widget.devicesList.length,
+          itemBuilder: (context, index) {
+            return Card(
               child: ListTile(
-            onTap: () => setState(
-                () => startDataStream(_services[index].characteristics)),
-            title: Text(_services[index].uuid.toString().contains(BLE_SERVICE)
-                ? BLE_SERVICE
-                : "NO"),
-          ));
-        });
+                  onTap: () => connectDevice(widget.devicesList[index]),
+                  title: Text(widget.devicesList[index].name),
+                  subtitle: Text(widget.devicesList[index].id.toString())),
+            );
+          }),
+    );
   }
 
-  ListView buildListViewOfCharacteristics() {
-    return ListView.builder(
-        itemCount: _values.length,
-        itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(
-            title: Text(_values[index].toString()),
-          ));
-        });
+  Scaffold buildRecordingMenu() {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Start"),
+                onPressed: () => startDataStream(),
+              ),
+              RaisedButton(
+                  child: Text("Stop"), onPressed: () => stopDataStream())
+            ]),
+      ),
+    );
   }
 
-  ListView _buildView() {
+  Scaffold _buildScaffold() {
     if (_connectedDevice != null) {
-      if (_values.isNotEmpty)
-        return buildListViewOfCharacteristics();
-      else
-        return buildListViewOfConnectedDevice();
+      return buildRecordingMenu();
     }
     return buildListViewOfDevices();
   }
@@ -255,13 +308,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: _buildView(),
-    );
+    return _buildScaffold();
   }
 }
