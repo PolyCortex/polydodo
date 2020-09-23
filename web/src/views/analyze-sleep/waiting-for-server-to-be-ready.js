92c7571e95d98689b260a8b9844c17c3afb9a3e1
Anes Belfodil <abelfodil@users.noreply.github.com>
2020-09-23 15:32:35 -0400

    Add upload sleep page (#22)

---
+++ web/src/views/analyze-sleep/waiting-for-server-to-be-ready.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WaitingForServerToBeReady = () => (
+  <div>
+    <h1>Waiting for local server to be running...</h1>
+  </div>
+);
+
+export default WaitingForServerToBeReady;
