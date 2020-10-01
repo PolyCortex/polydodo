353196b51f48870d9046c8aea0643956e9ffeae3
William Harvey <william.harvey@polymtl.ca>
2020-10-01 15:22:09 -0400

    add host environment variable for grpc

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -8,8 +8,7 @@ import 'src/app.dart';
 void main() async {
   // GRPC TEST
   final channel = ClientChannel(
-    '10.0.2.2', // Emulator alias to host's loopback interface (when server is running on computer)
-    // 'localhost', // When server is running on phone
+    const String.fromEnvironment('HOST'),
     port: 9090,
     options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
   );
