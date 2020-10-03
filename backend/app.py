b8918cada2483a111ed9f016ce9624ae06848d43
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-10-03 17:13:09 -0400

    added classification service to backend

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,44 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter server."""
-
 from concurrent import futures
 import logging
+import os
 
 import grpc
 
-from protos import helloworld_pb2
-from protos import helloworld_pb2_grpc
+from config.constants import (
+    SERVER_MAX_NB_WORKERS,
+    SERVER_PORT,
+    LOGGER_CONFIG,
+)
+from protos.polydodo.classification_pb2_grpc import (
+    ClassificationServiceServicer,
+    add_ClassificationServiceServicer_to_server
+)
+from protos.polydodo.classification_pb2 import GetSleepStagesResponse
 
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+class ClassificationService(ClassificationServiceServicer):
+    def GetSleepStages(self, request, context):
+        return GetSleepStagesResponse()
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:9090')
+    logging.basicConfig(**{
+        key: os.getenv(f'LOGGING_{key}', LOGGER_CONFIG[key])
+        for key in LOGGER_CONFIG
+    })
+
+    server = grpc.server(futures.ThreadPoolExecutor(
+        max_workers=SERVER_MAX_NB_WORKERS))
+
+    add_ClassificationServiceServicer_to_server(
+        ClassificationService(), server)
+
+    server.add_insecure_port(f'[::]:{SERVER_PORT}')
     server.start()
+    logging.info("Server started")
+
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
---
+++ backend/config/constants.py
@@ -0,0 +1,10 @@
+import logging
+
+
+SERVER_MAX_NB_WORKERS = 10
+SERVER_PORT = 9090
+
+LOGGER_CONFIG = {
+    'level': logging.INFO,
+    'format': "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+}
