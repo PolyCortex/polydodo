93d6d9f41b43b51e447aab1aba91dbe3d27fc97c
Anes Belfodil <abelfodil@users.noreply.github.com>
2020-10-08 12:10:49 -0400

    Add file upload (#30)
    * Add file upload
    
    * cleanup
    
    * Cleanup
    
    * max char per line => 120

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,43 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from waitress import serve
+from http import HTTPStatus
+
 
 app = Flask(__name__)
-cors = CORS(app)
+
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'txt', 'csv'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+
+@app.route('/analyze_sleep', methods=['POST'])
+def analyze_sleep():
+    if 'file' not in request.files:
+        return 'Missing file', HTTPStatus.BAD_REQUEST
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file', HTTPStatus.BAD_REQUEST
+
+    if not allowed_file(file.filename):
+        return 'File format not allowed', HTTPStatus.BAD_REQUEST
+
+    file_content = file.read()
+    form_data = request.form.to_dict()
+
+    return ''
+
+
+CORS(app,
+     resources={r'/*': {"origins": '*'}},
+     allow_headers='Content-Type')
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 serve(app, host='0.0.0.0', port=8080)
