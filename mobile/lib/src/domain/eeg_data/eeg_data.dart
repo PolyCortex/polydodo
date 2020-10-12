987a0b71d2b359abc323c09052bd41a15db6c425
Mourad Lachhab <mourad.lachhab@yahoo.com>
2020-10-12 13:31:18 -0400

    Switch from flutter_blue to flutter_reactive_ble (#35)
    * Remove wallets
    
    * inherit from entity
    
    * Normalize await usage
    
    * Switched to flutter_reactive_ble for less packet loss (clean needs to be cleaned)
    
    * Handling errors and removed packet loss variables
    
    * Rearranged pubspec, changed future void to void, and fixed cancel stream order
    
    * Changed for ios build and removed unecessary addins in android manifest
    
    * Add podfile
    
    * Revert "Add podfile"
    
    This reverts commit 5bcce2931714819804f85632a066e854eeb58016.
    
    * try something else
    
    Co-authored-by: Anes Belfodil <ans.belfodil@gmail.com>

--- mobile/lib/src/domain/eeg_data/eeg_data.dart
+++ mobile/lib/src/domain/eeg_data/eeg_data.dart
@@ -2,8 +2,7 @@
 import 'package:polydodo/src/domain/entity.dart';
 
 class EEGData extends Entity {
-  List<List> _values;
-  int sampleCounter = 0;
+  final List<List> _values;
 
   EEGData(id, this._values)
       : assert(_values != null),
