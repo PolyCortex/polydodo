bb32ccaf92a9cf30a0a2419b2f1fbebd691fd2a2
Mathieu Bélanger <mathieu.belanger@logient.com>
2020-10-16 12:21:04 -0400

    Fixing conflicts with bluetooth merge

--- mobile/lib/src/app.dart
+++ mobile/lib/src/app.dart
@@ -1,8 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polydodo/src/presentation/bluetooth_route/bluetoothSelector_route.dart';
 
 import 'locator.dart';
+import 'presentation/navigation/routes/router.gr.dart' as auto_router;
 import 'theme.dart';
 
 class App extends StatelessWidget {
@@ -18,10 +20,14 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'PolyDodo',
         theme: theme,
-        initialRoute: BluetoothSelectorRoute.name,
+        builder: ExtendedNavigator.builder<auto_router.Router>(
+            router: auto_router.Router()),
+        /*home: WalletsRoute(),
+        initialRoute: WalletsRoute.name,
         routes: {
           BluetoothSelectorRoute.name: (context) => BluetoothSelectorRoute(),
-        },
+          WalletsRoute.name: (context) => WalletsRoute(), 
+        }, */
       ),
     );
   }
