e7f35f7dd02756921f75604d12d49e3e8aa565c7
Mathieu Bélanger <mathieu.belanger@logient.com>
2020-10-22 00:11:49 -0400

    adding dashboard page

---
+++ mobile/lib/src/presentation/pages/dashboard/dashboard_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:polydodo/src/presentation/navigation/navdrawer_widget.dart';
+
+class DashBoardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Polydodo')),
+      drawer: NavDrawerPage(),
+      body: Center(
+        child: Text("Allo"),
+      ),
+    );
+  }
+}
