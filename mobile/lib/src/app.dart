16bbe80ca75be24e07196503cfc7b0c14b0d2aa0
Mathieu Bélanger <mathieu.belanger@logient.com>
2020-10-22 00:13:01 -0400

    unused import

--- mobile/lib/src/app.dart
+++ mobile/lib/src/app.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:polydodo/src/presentation/bluetooth_route/bluetoothSelector_route.dart';
 
 import 'locator.dart';
 import 'presentation/navigation/routes/router.gr.dart' as auto_router;
