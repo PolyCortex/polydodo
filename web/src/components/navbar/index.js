0c0d7d0bb3689a3106976b28bf6b5ae69009c155
William Harvey <william.harvey@polymtl.ca>
2020-10-24 13:53:14 -0400

    convert Navbar Class component to function using hooks

--- web/src/components/navbar/index.js
+++ web/src/components/navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Headroom from 'headroom.js';
@@ -35,77 +35,72 @@ OutsideLink.defaultProps = {
   tooltipText: '',
 };
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.navbarstrapRef = React.createRef();
-  }
-
-  componentDidMount() {
-    const headroom = new Headroom(this.navbarstrapRef);
+const Navbar = () => {
+  const navbarstrapRef = useRef();
+  useEffect(() => {
+    const headroom = new Headroom(navbarstrapRef.current);
     headroom.init();
-  }
+    return () => headroom.destroy();
+  });
 
-  render() {
-    return (
-      <div ref={(ref) => (this.navbarstrapRef = ref)} className="navbar-main navbar-transparent navbar-light headroom">
-        <NavbarStrap id="navbar-main" expand="lg">
-          <Container>
-            <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
-              <img alt="Polydodo" src={Logo} />
-            </NavbarBrand>
+  return (
+    <div ref={(ref) => (navbarstrapRef.current = ref)} className="navbar-main navbar-transparent navbar-light headroom">
+      <NavbarStrap id="navbar-main" expand="lg">
+        <Container>
+          <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+            <img alt="Polydodo" src={Logo} />
+          </NavbarBrand>
 
-            <Nav className="navbar-nav-hover align-items-lg-center" navbar>
-              <NavItem>
-                <NavLink to="/" tag={Link}>
-                  {text['navbar_home']}
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/record-my-sleep" tag={Link}>
-                  {text['navbar_record']}
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/analyze-my-sleep" tag={Link}>
-                  {text['navbar_analyze']}
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/performance" tag={Link}>
-                  {text['navbar_performance']}
-                </NavLink>
-              </NavItem>
-            </Nav>
+          <Nav className="navbar-nav-hover align-items-lg-center" navbar>
+            <NavItem>
+              <NavLink to="/" tag={Link}>
+                {text['navbar_home']}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/record-my-sleep" tag={Link}>
+                {text['navbar_record']}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/analyze-my-sleep" tag={Link}>
+                {text['navbar_analyze']}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/performance" tag={Link}>
+                {text['navbar_performance']}
+              </NavLink>
+            </NavItem>
+          </Nav>
 
-            <Nav className="align-items-lg-center ml-lg-auto" navbar>
-              <OutsideLink
-                href="https://www.facebook.com/PolyCortex/"
-                iconClass="fa-facebook-square"
-                linkName="Facebook"
-                tooltipText={text['navbar_facebook_tooltip']}
-                tooltipID="tooltip333589074"
-              />
-              <OutsideLink
-                href="https://www.instagram.com/polycortex/"
-                iconClass="fa-instagram"
-                linkName="Instagram"
-                tooltipText={text['navbar_instagram_tooltip']}
-                tooltipID="tooltip356693867"
-              />
-              <OutsideLink
-                href="https://github.com/PolyCortex"
-                iconClass="fa-github"
-                linkName="Github"
-                tooltipText={text['navbar_github_tooltip']}
-                tooltipID="tooltip112445449"
-              />
-            </Nav>
-          </Container>
-        </NavbarStrap>
-      </div>
-    );
-  }
-}
+          <Nav className="align-items-lg-center ml-lg-auto" navbar>
+            <OutsideLink
+              href="https://www.facebook.com/PolyCortex/"
+              iconClass="fa-facebook-square"
+              linkName="Facebook"
+              tooltipText={text['navbar_facebook_tooltip']}
+              tooltipID="tooltip333589074"
+            />
+            <OutsideLink
+              href="https://www.instagram.com/polycortex/"
+              iconClass="fa-instagram"
+              linkName="Instagram"
+              tooltipText={text['navbar_instagram_tooltip']}
+              tooltipID="tooltip356693867"
+            />
+            <OutsideLink
+              href="https://github.com/PolyCortex"
+              iconClass="fa-github"
+              linkName="Github"
+              tooltipText={text['navbar_github_tooltip']}
+              tooltipID="tooltip112445449"
+            />
+          </Nav>
+        </Container>
+      </NavbarStrap>
+    </div>
+  );
+};
 
 export default Navbar;
