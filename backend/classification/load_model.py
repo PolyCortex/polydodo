26be93026bc8b1a890c23c59ac551f76ca236ebd
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-01 00:11:38 -0400

    added check to see it local object modified date is after remote object update date

--- backend/classification/load_model.py
+++ backend/classification/load_model.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from os import path
+import re
 import sys
+import xml.etree.ElementTree as ET
+
+from pytz import utc
 from requests import get
 import onnxruntime
-import re
-import xml.etree.ElementTree as ET
 
 SCRIPT_PATH = path.dirname(path.realpath(sys.argv[0]))
 MODEL_FILENAME = 'model.onnx'
@@ -18,7 +21,7 @@ def _download_file(url, output):
         f.write(get(url).content)
 
 
-def _get_latest_object_size(bucket_url, filename):
+def _get_latest_object_information(bucket_url, filename):
     raw_result = get(bucket_url).text
     # https://stackoverflow.com/a/15641319
     raw_result = re.sub(' xmlns="[^"]+"', '', raw_result)
@@ -26,13 +29,24 @@ def _get_latest_object_size(bucket_url, filename):
     objects_nodes = result_root_node.findall('Contents')
     object_node = [object_node for object_node in objects_nodes if object_node.find("Key").text == filename][0]
     object_size = int(object_node.find("Size").text)
-    return object_size
+    object_latest_update = datetime.strptime(object_node.find("LastModified").text, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+    return {'size': object_size, 'latest_update': object_latest_update}
+
+
+def _has_latest_model():
+    latest_model_information = _get_latest_object_information(BUCKET_URL, MODEL_FILENAME)
+    current_model_size = path.getsize(MODEL_PATH)
+    current_model_update = utc.localize(datetime.fromtimestamp(path.getmtime(MODEL_PATH)))
+
+    return (
+        current_model_update >= latest_model_information['latest_update']
+        and current_model_size == latest_model_information['size']
+    )
 
 
 def load_model():
-    if path.exists(MODEL_PATH):
-        print(f"Model is already downloaded at {MODEL_PATH}, checking if it's the latest.")
-    if not path.exists(MODEL_PATH) or path.getsize(MODEL_PATH) != _get_latest_object_size(BUCKET_URL, MODEL_FILENAME):
+    if not path.exists(MODEL_PATH) or not _has_latest_model():
         print("Downloading latest model...")
         _download_file(MODEL_URL, MODEL_PATH)
     print("Loading model...")
