acb8494321f46044dacab28c09ce75be8a80fb0a
Mathieu Bélanger <mbelanger.poly@gmail.com>
2020-11-03 09:29:05 -0500

    Update router.dart

--- mobile/lib/src/presentation/navigation/routes/router.dart
+++ mobile/lib/src/presentation/navigation/routes/router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route_annotations.dart';
-import 'package:polydodo/src/presentation/navigation/navdrawer_widget.dart';
 import 'package:polydodo/src/presentation/pages/bluetooth_page/bluetoothSelector_page.dart';
 import 'package:polydodo/src/presentation/pages/dashboard/dashboard_page.dart';
 import 'package:polydodo/src/presentation/pages/record_sleep/record_sleep_guide_page.dart';
