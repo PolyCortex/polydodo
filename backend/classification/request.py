7fe5a004c3c172e12d097c15fbe92ca768356a3e
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-05 23:37:23 -0500

    moved validation to request class

--- backend/classification/request.py
+++ backend/classification/request.py
@@ -1,6 +1,14 @@
 
 from classification.config.constants import EPOCH_DURATION
-from classification.validation import validate
+from classification.config.constants import (
+    FILE_MINIMUM_DURATION,
+    ACCEPTED_AGE_RANGE,
+)
+from classification.exceptions import (
+    TimestampsError,
+    FileSizeError,
+    ClassificationError,
+)
 
 
 class ClassificationRequest():
@@ -30,4 +38,35 @@ class ClassificationRequest():
         return (self.wakeup - self.bedtime) / EPOCH_DURATION
 
     def validate(self):
-        validate(self.raw_eeg, self)
+        self._validate_timestamps()
+        self._validate_file_with_timestamps()
+        self._validate_age()
+
+    def _validate_timestamps(self):
+        has_positive_timespan = self.bedtime > self.stream_start and self.wakeup > self.stream_start
+        has_got_out_of_bed_after_in_bed = self.wakeup > self.bedtime
+        has_respected_minimum_bed_time = (self.wakeup - self.bedtime) > FILE_MINIMUM_DURATION
+
+        if not(
+            has_positive_timespan
+            and has_got_out_of_bed_after_in_bed
+            and has_respected_minimum_bed_time
+        ):
+            raise TimestampsError()
+
+    def _validate_file_with_timestamps(self):
+        has_raw_respected_minimum_file_size = self.raw_eeg.times[-1] > FILE_MINIMUM_DURATION
+
+        if not has_raw_respected_minimum_file_size:
+            raise FileSizeError()
+
+        is_raw_at_least_as_long_as_out_of_bed = self.raw_eeg.times[-1] >= self.out_of_bed_seconds
+
+        if not is_raw_at_least_as_long_as_out_of_bed:
+            raise TimestampsError()
+
+    def _validate_age(self):
+        is_in_accepted_range = ACCEPTED_AGE_RANGE[0] <= int(self.age) <= ACCEPTED_AGE_RANGE[1]
+
+        if not(is_in_accepted_range):
+            raise ClassificationError('invalid age')
