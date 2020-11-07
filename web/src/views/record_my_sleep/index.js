aaa925f128a17aa7dc05551bcb3a4d58f7947d27
Anes Belfodil <ans.belfodil@gmail.com>
2020-11-07 08:54:05 -0500

    Add hovering cards

--- web/src/views/record_my_sleep/index.js
+++ web/src/views/record_my_sleep/index.js
@@ -103,34 +103,6 @@ const RecordMySleep = () => {
             </Row>
           </div>
 
-          <Container className="text-justify pt-lg-5 pb-lg-5">
-            <CardDeck>
-              <Card className="card-lift--hover shadow border-0">
-                <CardBody className="py-5">
-                  <h6 className="text-warning text-uppercase">Ensure good skin contact</h6>
-                  <p className="mt-3">
-                    To ensure that skin contact is good, it is important to measure the impedance between the
-                    electrodes. Start by measuring the impedance between your active electrode and its reference (e.g.:
-                    Fpz and Cz). Also, check the impedance between the reference electrodes of each channel (Cz and Oz)
-                    and the ground electrode (A2). As a rule of thumb, low impedance (10 KOhms) is good and high ones
-                    are bad (let’s say 150 KOhms).
-                  </p>
-                </CardBody>
-              </Card>
-              <Card className="card-lift--hover shadow border-0">
-                <CardBody className="py-5">
-                  <h6 className="text-primary text-uppercase">Limit noise</h6>
-                  <p className="mt-3">
-                    Tie the electrodes together with a hair tie, and if you have long hair, tie them all together in
-                    order to avoid static during the night. To limit electromagnetic interferences you may want to place
-                    your board in some sort of Faraday cage, for example, a plastic container wrapped up in aluminium
-                    foil.
-                  </p>
-                </CardBody>
-              </Card>
-            </CardDeck>
-          </Container>
-
           <div>
             <h4 className="display-4">Wire the electrodes to the OpenBCI device</h4>
             <p className="lead">Here’s is how you need to wire your electrodes to the OpenBCI board you are using:</p>
@@ -148,8 +120,38 @@ const RecordMySleep = () => {
         </Container>
       </section>
 
+      <section className="section section-lg bg-gradient-default"></section>
+      <section className="section section-lg pt-lg-0 pb-lg-0 mt--200">
+        <Container className="text-justify pt-lg-5">
+          <CardDeck>
+            <Card className="card-lift--hover shadow border-0">
+              <CardBody className="py-5">
+                <h6 className="text-warning text-uppercase">Ensure good skin contact</h6>
+                <p className="mt-3">
+                  To ensure that skin contact is good, it is important to measure the impedance between the electrodes.
+                  Start by measuring the impedance between your active electrode and its reference (e.g.: Fpz and Cz).
+                  Also, check the impedance between the reference electrodes of each channel (Cz and Oz) and the ground
+                  electrode (A2). As a rule of thumb, low impedance (10 KOhms) is good and high ones are bad (let’s say
+                  150 KOhms).
+                </p>
+              </CardBody>
+            </Card>
+            <Card className="card-lift--hover shadow border-0">
+              <CardBody className="py-5">
+                <h6 className="text-primary text-uppercase">Limit noise</h6>
+                <p className="mt-3">
+                  Tie the electrodes together with a hair tie, and if you have long hair, tie them all together in order
+                  to avoid static during the night. To limit electromagnetic interferences you may want to place your
+                  board in some sort of Faraday cage, for example, a plastic container wrapped up in aluminium foil.
+                </p>
+              </CardBody>
+            </Card>
+          </CardDeck>
+        </Container>
+      </section>
+
       <section className="section section-lg">
-        <Container className="text-justify pt-lg">
+        <Container className="text-justify">
           <h3 className="display-3">How to use OpenBCI GUI</h3>
           <p className="lead">
             First thing you need is the OpenBCI GUI in order to use the hardware. Therefrom, select{' '}
