f558366738c953124ec4ec351f80b858991736b5
MouradLachhab <mourad.lachhab@yahoo.com>
2020-11-10 11:16:55 -0500

    Removed unused import

--- mobile/lib/src/presentation/pages/sleep_sequence_stats_page/sleep_sequence_stats_page.dart
+++ mobile/lib/src/presentation/pages/sleep_sequence_stats_page/sleep_sequence_stats_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polydodo/src/application/sleep_sequence_stats/sleep_sequence_stats_cubit.dart';
 import 'package:polydodo/src/application/sleep_sequence_stats/sleep_sequence_stats_state.dart';
-import 'package:polydodo/src/presentation/navigation/navdrawer_tabs.dart';
 import 'package:polydodo/src/presentation/pages/sleep_sequence_stats_page/app_bar.dart';
 import 'package:polydodo/src/presentation/pages/sleep_sequence_stats_page/metric_section.dart';
 import 'package:polydodo/src/presentation/pages/sleep_sequence_stats_page/sleep_stages_section.dart';
