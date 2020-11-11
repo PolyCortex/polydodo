Fix scaling bug

backend/classification/parser/__init__.py
@@ -12,11 +12,9 @@ The Cyton board logging format is also described here:
 """
 from mne import create_info
 from mne.io import RawArray
-import numpy as np
 
 from classification.config.constants import OPENBCI_CYTON_SAMPLE_RATE, EEG_CHANNELS
-from classification.parser.constants import SCALE_V_PER_COUNT
-from classification.parser.file_type import FileType, detect_file_type
+from classification.parser.file_type import detect_file_type
 
 
 def get_raw_array(file):
@@ -36,7 +34,7 @@ def get_raw_array(file):
     eeg_raw = parse(file)
 
     raw_object = RawArray(
-        SCALE_V_PER_COUNT * np.transpose(eeg_raw),
+        eeg_raw,
         info=create_info(
             ch_names=EEG_CHANNELS,
             sfreq=OPENBCI_CYTON_SAMPLE_RATE,

backend/classification/parser/sd_file.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from classification.parser.constants import RETAINED_COLUMNS
 from classification.parser.csv import read_csv
+from classification.parser.constants import SCALE_V_PER_COUNT
 
 ROWS_TO_SKIP = 2
 
@@ -26,5 +27,5 @@ def parse_sd_file(file):
     eeg_raw = read_csv(file, ROWS_TO_SKIP, RETAINED_COLUMNS)
     hexstr_to_int = np.vectorize(_hexstr_to_int)
     eeg_raw = hexstr_to_int(eeg_raw)
-
+    eeg_raw = SCALE_V_PER_COUNT * np.transpose(eeg_raw),
     return eeg_raw

backend/classification/parser/session_file.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 
 from classification.parser.constants import RETAINED_COLUMNS
 from classification.parser.csv import read_csv
@@ -14,4 +14,6 @@ def parse_session_file(file):
     Returns:
     - np.array of the two EEG channels of interest
     """
-    return read_csv(file, ROWS_TO_SKIP, RETAINED_COLUMNS)
+    eeg_raw = read_csv(file, ROWS_TO_SKIP, RETAINED_COLUMNS)
+    eeg_raw = np.transpose(eeg_raw) / 1000
+    return eeg_raw