722d5e767265eb8a36cadffd3248d9fbd6306e3b
Anes Belfodil <ans.belfodil@gmail.com>
2020-11-11 15:20:15 -0500

    Fix volt conversion

--- backend/classification/parser/constants.py
+++ backend/classification/parser/constants.py
@@ -3,7 +3,8 @@ from classification.config.constants import EEG_CHANNELS
 ADS1299_Vref = 4.5
 ADS1299_gain = 24.
 SCALE_uV_PER_COUNT = ADS1299_Vref / ((2**23) - 1) / ADS1299_gain * 1000000
-SCALE_V_PER_COUNT = SCALE_uV_PER_COUNT / 1e6
+uV_TO_V = 1 / 1e6
+SCALE_V_PER_COUNT = SCALE_uV_PER_COUNT * uV_TO_V
 
 FILE_COLUMN_OFFSET = 1
 
