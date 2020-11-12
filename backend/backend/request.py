5d651434bf8bfa330cdc6f8cd0d89380b3a09197
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-11 20:33:55 -0500

    extracted stream duration

--- backend/backend/request.py
+++ backend/backend/request.py
@@ -19,10 +19,13 @@ class ClassificationRequest():
         self.bedtime = bedtime
         self.wakeup = wakeup
         self.raw_eeg = raw_eeg
-        self.stream_duration = stream_duration if stream_duration else self.raw_eeg.times[-1] + (
-            1 / self.raw_eeg.info['sfreq'])
+        self.stream_duration = stream_duration if stream_duration else self._get_stream_duration()
 
         self._validate()
+
+    def _get_stream_duration(self):
+        PERIOD_DURATION = 1 / self.raw_eeg.info['sfreq']
+        return self.raw_eeg.times[-1] + PERIOD_DURATION
 
     @property
     def in_bed_seconds(self):
