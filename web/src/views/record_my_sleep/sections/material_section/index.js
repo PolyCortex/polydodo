859125c95eabb415aca0ff1a6dc3af469937c6f7
William Harvey <william.harvey@polymtl.ca>
2020-11-12 16:15:17 -0500

    change sd card size and material content

--- web/src/views/record_my_sleep/sections/material_section/index.js
+++ web/src/views/record_my_sleep/sections/material_section/index.js
@@ -12,7 +12,7 @@ const MaterialSection = () => (
       <p className="lead">
         If you already own an OpenBCI board and some basic EEG equipment, you could have very little to buy except a few
         things from the drugstore. Some of the brands that we mention have their equivalent and can be replaced. We do
-        not advertise, but we have used these products and found them useful and of good quality.
+        not advertise the mentionned products, but we have used them and found them useful and of good quality.
       </p>
       <AlternatingTextImage elements={materialNeeded.elements} />
     </Container>
