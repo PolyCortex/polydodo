85ab1f6c128d3edb3bbdaefda02baac2a59b7617
William Harvey <william.harvey@polymtl.ca>
2020-11-15 00:37:10 -0500

    How to use OpenBCI GUI

--- web/src/views/record_my_sleep/sections/gui_configuration_section/index.js
+++ web/src/views/record_my_sleep/sections/gui_configuration_section/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Alert, Col, Container, Row } from 'reactstrap';
 import Table from 'components/table';
 
 const GUIConfigurationSection = () => (
@@ -7,17 +7,32 @@ const GUIConfigurationSection = () => (
     <Container className="text-justify">
       <h3 className="display-3">How to use OpenBCI GUI</h3>
       <p className="lead">
-        First thing you need is the OpenBCI GUI in order to use the hardware. Therefrom, select{' '}
-        <b>Live (from &lt;boardname&gt;)</b> as your source. If you’re using a Cyton board we suggest you use a microSD
-        card using FAT32 format. If you do so, do not forget to select at least <b>12 hour maximum</b> for the write to
-        SD card parameter. Otherwise, you must keep your computer near and open for the whole night and log the data to
-        a session file. If you do it this way, do not forget to select the OpenBCI file output and to select{' '}
-        <b>No Limit</b> as your max file duration. When everything is configured properly, you can press{' '}
+        First thing you need is the&nbsp;
+        <strong>
+          <a href="https://openbci.com/index.php/downloads" target="_blank" rel="noreferrer">
+            OpenBCI GUI
+          </a>
+        </strong>
+        &nbsp;in order to use the hardware. Therefrom, select <b>Live (from &lt;boardname&gt;)</b> as your source. If
+        you’re using a Cyton board we suggest you use a microSD card using FAT32 format. If you do so, do not forget to
+        select at least&nbsp;
+        <b>12 hour maximum</b> for the write to SD card parameter.
+      </p>
+      <Alert color="warning mt-5">
+        <span className="text-white alert-inner--text">
+          <i className="fas fa-sd-card fa-lg mr-2"></i>
+          <strong>Despite the fact that the Ganglion has a slot to receive a microsd card, it cannot be used.</strong>
+          &nbsp;This was a bonus feature to their Kickstarter campaign, but eventually the feature has been put aside.
+        </span>
+      </Alert>
+      <p className="lead mt-5">
+        Otherwise, you must keep your computer near and open for the whole night and log the data to a session file. If
+        you do it this way, do not forget to select the <strong>OpenBCI file output</strong> and to select&nbsp;
+        <b>No Limit</b> as your max file duration. When everything is configured properly, you can press&nbsp;
         <b>Start Session</b>. Before pressing <b>Start Stream</b>, press <b>Hardware Settings</b> and configure
         everything as follow:
       </p>
-
-      <Row>
+      <Row className="my-5">
         <Col>
           <h4 className="display-4">Cyton</h4>
           <Table
@@ -39,7 +54,6 @@ const GUIConfigurationSection = () => (
           />
         </Col>
       </Row>
-
       <p className="lead">
         Do not forget to deactivate all unused channels by greying out the numbered colored pill on the left of the
         signal in the GUI. You can now press <b>Start data stream</b>. If you are using a microSD card and a Cyton
