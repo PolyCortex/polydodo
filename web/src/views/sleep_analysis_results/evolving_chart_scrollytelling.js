ff7484970367bee5114cd8553dff27b4a13dc266
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-21 13:06:43 -0500

    moved barchart cards to right && added bootstrap rows

--- web/src/views/sleep_analysis_results/evolving_chart_scrollytelling.js
+++ web/src/views/sleep_analysis_results/evolving_chart_scrollytelling.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Card, CardBody } from 'reactstrap';
+import { Container, Card, CardBody, Row } from 'reactstrap';
 import _ from 'lodash';
 
 import D3ComponentScrollyTelling from 'components/d3component_scrollytelling';
@@ -18,21 +18,6 @@ import './style.css';
 
 const EvolvingChartScrollyTelling = ({ epochs, report, metadata }) => {
   const [isInitialized, setIsInitialized] = useState(false);
-  console.log(report, metadata);
-
-  const getTimeString = (numberSecondsUTC) => (
-    <span className="text-primary">{new Date(numberSecondsUTC * 1000).toLocaleTimeString()}</span>
-  );
-  const getDurationString = (numberSeconds) => {
-    const nbHours = Math.floor(numberSeconds / 3600);
-    const nbMinutes = Math.floor((numberSeconds % 3600) / 60);
-
-    return (
-      <span className="text-primary">
-        {nbHours > 0 ? `${nbHours} hours and ${nbMinutes} minutes` : `${nbMinutes} minutes`}
-      </span>
-    );
-  };
 
   const { bedTime, wakeUpTime, totalBedTime } = metadata;
   const {
@@ -51,6 +36,20 @@ const EvolvingChartScrollyTelling = ({ epochs, report, metadata }) => {
   const numberStagesTraversed = _.filter(sleepStageTimes, (time) => time > 0).length;
   const mostProminentStage = _.maxBy(_.keys(sleepStageTimes), (stage) => sleepStageTimes[stage]);
 
+  const Metric = ({ children }) => (
+    <span className="text-primary">
+      <strong>{children}</strong>
+    </span>
+  );
+
+  const getTimeString = (numberSecondsUTC) => <Metric>{new Date(numberSecondsUTC * 1000).toLocaleTimeString()}</Metric>;
+  const getDurationString = (numberSeconds) => {
+    const nbHours = Math.floor(numberSeconds / 3600);
+    const nbMinutes = Math.floor((numberSeconds % 3600) / 60);
+
+    return <Metric>{nbHours > 0 ? `${nbHours} hours and ${nbMinutes} minutes` : `${nbMinutes} minutes`}</Metric>;
+  };
+
   return (
     <Container className="evolving_chart__container">
       <div style={{ position: 'sticky', top: '10%' }}>
@@ -62,97 +61,124 @@ const EvolvingChartScrollyTelling = ({ epochs, report, metadata }) => {
         />
       </div>
       <div style={{ marginBottom: '50%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>
-            We can see that each colored block represents a part of your night. They are ordered from bed time to out of
-            bed timestamps you’ve written in your journal. Each color is associated with a specific sleep stage. You
-            went to bed at {getTimeString(bedTime)} and you got out of bed at {getTimeString(wakeUpTime)}, which adds up
-            to {getDurationString(totalBedTime)} of time spent in bed. Of this total time, you spent{' '}
-            {getDurationString(efficientSleepTime)} actually sleeping. You first fell asleep at{' '}
-            {getTimeString(sleepOnset)}, to which we will refer to as sleep onset. The last non wake block ended at{' '}
-            {getTimeString(sleepOffset)}, which can also be referred to as sleep offset. During that night's sleep, you
-            went through {numberStagesTraversed} sleep stages. Let's try to see a little better what happened about each
-            of them.
-          </p>
-        </CardBody>
-      </Card>
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              We can see that each colored block represents a part of your night. This timeline starts at your bed time,
+              so {getTimeString(bedTime)}, and ends at the time you got out of bed, whereas {getTimeString(wakeUpTime)}.
+              Of this total time, you spent {getDurationString(efficientSleepTime)} actually sleeping. You first fell
+              asleep at {getTimeString(sleepOnset)}, to which we will refer to as sleep onset. You woke up at{' '}
+              {getTimeString(sleepOffset)}, which can also be referred to as sleep offset. During that night's sleep,
+              you went through {numberStagesTraversed} different stages. Let's take a look at them.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
       {isInitialized && (
         <WaypointDirection onDown={instanceChartCallbacks.fromTimeline} onUp={timelineChartCallbacks.fromInstance} />
       )}
       <div style={{ marginBottom: '125%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>Wake stage is of course the stage we want to minimize when in bed. It can be decomposed into two parts:</p>
-          <ul>
-            <li>
-              Sleep latency : Time spent before falling asleep, which corresponds to {getDurationString(sleepLatency)}
-              &nbsp;minutes in your case.
-            </li>
-            <li>
-              Wake after sleep onset (WASO): Time spent awake after first falling asleep and before waking up, which
-              corresponds to {getDurationString(WASO)}.
-            </li>
-          </ul>
-          <p>
-            For healthy adults, it is normal to experience small awakenings during the night. Unprovoked awakenings are
-            mostly during or after REM stages.
-          </p>
-        </CardBody>
-      </Card>
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              You spent {getDurationString(report.WTime)} awake through your night. This duration is, in a sleep report,
+              usually decomposed as three different components.
+            </p>
+            <p>
+              First, <strong>sleep latency</strong> corresponds to the time you spend in bed before first falling
+              asleep, which here is {getDurationString(sleepLatency)}.
+            </p>
+            <p>
+              Secondly, <strong>wake after sleep onset</strong>, often abbreviated as WASO, is the time spent awake
+              after first falling asleep and before waking up. In your case, it corresponds to {getDurationString(WASO)}
+              .
+            </p>
+            <p>
+              Thirdly, the time you spend in bed after waking up and getting out of bed, here being{' '}
+              {getDurationString(wakeAfterSleepOffset)}, is called <strong>wake after sleep offset</strong>.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
       <div style={{ marginBottom: '125%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>
-            <strong>REM stage</strong> stands for “Rapid Eyes Movements” and is also known as “paradoxical sleep”. It is
-            associated with dreaming and, as the National Sleep Foundation says,{' '}
-            <strong>“the brain is awake and body paralyzed.”</strong>
-          </p>
-          <p>
-            <strong>N1 stage</strong> is associated with that drowsy feeling before falling asleep. Most people wouldn’t
-            say they fell asleep if they’ve been woken up from N1 sleep.
-          </p>
-          <p>
-            <strong>N2 stage</strong> still corresponds to a light sleep, but where the muscle activity decreases more,
-            and the eyes have stopped moving. It is called, along with N1, <strong>light sleep</strong>.
-          </p>
-          <p>
-            <strong>N3 stage</strong> is when you are deeply asleep, hence it’s also called <strong>deep sleep</strong>,
-            or sometimes <strong>slow wave</strong> sleep, and is the most difficult to wake up from. It is during those
-            stages that your cells get repaired, and that tissue grows. But how much time did you spend in each stage
-            during the whole night?
-          </p>
-        </CardBody>
-      </Card>
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              <strong>REM stage</strong>, which is short for <i>rapid eyes movement</i>, is the moment of the night
+              where you experience dreams. During this stage, your muscles should be completely immobilized, and on the
+              opposite, the activity of your brain should be very active, which illustrates why this stage is also
+              called paradoxical sleep.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
+      <div style={{ marginBottom: '100%' }} />
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              <strong>N1 stage</strong> is associated with that drowsy feeling before falling asleep. Most people
+              wouldn’t say they fell asleep if they’ve been woken up from N1 sleep.
+            </p>
+            <p>
+              <strong>N2 stage</strong> still corresponds to a light sleep, but where the muscle activity decreases
+              more, and the eyes have stopped moving. It is called, along with N1, <strong>light sleep</strong>.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
+      <div style={{ marginBottom: '100%' }} />
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              <strong>N3 stage</strong> is when you are deeply asleep, hence it’s also called{' '}
+              <strong>deep sleep</strong>, or sometimes <strong>slow wave sleep</strong>, and is the most difficult to
+              wake up from. It is during those stages that your cells get repaired, and that tissue grows.
+            </p>
+            <p>
+              We've looked at the different functions of each sleep stages. But how much time did you actually spend in
+              each during the night?
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
       {isInitialized && (
         <WaypointDirection onDown={barChartCallbacks.fromInstance} onUp={instanceChartCallbacks.fromBarChart} />
       )}
       <div style={{ marginBottom: '125%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>
-            From here, we can look at your sleep efficiency, which is the proportion of time spent asleep over the
-            overall time spent in bed. In your case, it corresponds to&nbsp;
-            <span className="text-primary">{(sleepEfficiency * 100).toFixed()}%</span>, or&nbsp;
-            {getDurationString(sleepEfficiency * totalBedTime)}.
-          </p>
-        </CardBody>
-      </Card>
+      <Row>
+        <Card className="shadow col-lg-6 ml-auto">
+          <CardBody>
+            <p>
+              We are currently looking at your in bed sleep stage proportions. Wake time may be overrepresented, because
+              it includes your sleep latency ({getDurationString(sleepLatency)}) and the time you spent in bed after
+              waking up ({getDurationString(wakeAfterSleepOffset)}).
+            </p>
+            <p>
+              We can see that your most prominent sleep stage is <Metric>{mostProminentStage}</Metric>, which in your
+              case corresponds to&nbsp;{getDurationString(sleepStageTimes[mostProminentStage])}. Usually, the most
+              prominent sleep stage is&nbsp;{mostProminentStage === 'N2' && ', as it is in your case, '}N2.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
       <div style={{ marginBottom: '125%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>
-            We are currently looking at your in bed sleep stage proportions. Wake time may be overrepresented, because
-            it includes your sleep latency ({getDurationString(sleepLatency)}) and the time you spent in bed after
-            waking up ({getDurationString(wakeAfterSleepOffset)}).
-          </p>
-          <p>
-            We can see that your most prominent sleep stage is {mostProminentStage}, which in your case corresponds
-            to&nbsp;{getDurationString(sleepStageTimes[mostProminentStage])}. Usually, the most prominent sleep stage
-            is&nbsp;{mostProminentStage === 'N2' && ', as it is in your case, '}N2.
-          </p>
-        </CardBody>
-      </Card>
+      <Row>
+        <Card className="shadow col-lg-6 ml-auto">
+          <CardBody>
+            <p>
+              From here, we can also look at your sleep efficiency, which is the proportion of time spent asleep over
+              the overall time spent in bed. In your case, it corresponds to&nbsp;
+              <span className="text-primary">{(sleepEfficiency * 100).toFixed()}%</span>, or&nbsp;
+              {getDurationString(sleepEfficiency * totalBedTime)}.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
       {isInitialized && (
         <WaypointDirection
           onDown={stackedBarChartCallbacks.fromBarChart}
@@ -160,16 +186,19 @@ const EvolvingChartScrollyTelling = ({ epochs, report, metadata }) => {
         />
       )}
       <div style={{ marginBottom: '125%' }} />
-      <Card className="shadow" style={{ position: 'relative' }}>
-        <CardBody>
-          <p>
-            As a rule of thumb, adults approximately stay 5% of their total sleep time in N1; 50% in N2; and 20% is in
-            N3. The remaining 25% is REM stage sleep.
-          </p>
-        </CardBody>
-      </Card>
-      <div style={{ marginBottom: '125%' }} />
-      &nbsp;
+      <Row>
+        <Card className="shadow col-lg-6 mx-auto">
+          <CardBody>
+            <p>
+              As a rule of thumb, adults approximately stay 5% of their total sleep time in N1; 50% in N2; and 20% is in
+              N3. The remaining 25% is REM stage sleep.
+            </p>
+          </CardBody>
+        </Card>
+      </Row>
+      <Row>
+        <div style={{ marginBottom: '70%' }} />
+      </Row>
     </Container>
   );
 };
