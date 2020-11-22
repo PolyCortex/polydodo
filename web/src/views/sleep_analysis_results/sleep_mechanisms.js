96121033e625fd212f974d75c930fd0f278712d6
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-22 10:43:50 -0500

    fixed cards responsive

--- web/src/views/sleep_analysis_results/sleep_mechanisms.js
+++ web/src/views/sleep_analysis_results/sleep_mechanisms.js
@@ -6,34 +6,34 @@ import FloatingCard from 'components/floating_card';
 import './style.css';
 
 const SleepMechanisms = () => (
-  <Container className="text-justify mt-6 mb--100">
+  <Container className="text-justify mt-6 mb--150">
     <CardDeck>
-      <Col lg="6">
+      <Col lg="6" className="mb-6">
         <FloatingCard
           cardClassName="bg-warning"
           headerText="Circadian Rhythms"
           bodyText={
-            <p>
+            <span>
               This is your biological clock! It is a self-sustained internal proccess responsable for regulating your
               sleep and wake cycles every 24 hours. This is why you feel sleepy at night and awake during the day. Some
               habits or events may have an influence on these rhythms. Sleep cycles then take place in these broader
               processes.
-            </p>
+            </span>
           }
         />
       </Col>
-      <Col lg="6">
+      <Col lg="6" className="mb-6">
         <FloatingCard
           cardClassName="bg-primary"
           headerText="Sleep Homeothesis"
           bodyText={
-            <p>
+            <span>
               Sleep homeostasis is a biological regulatory process that rules over how much sleep time and intensity you
               need to get. It is tightly coupled with the <q>sleep debt</q> concept as the more you are awake, the more
               and the deeper you'll need to eventually sleep. One of the main factors that explains this comes from the
               adenosine cycle. When you are awake, the level of adenosine continuously increases in the brain. When we
               sleep, it is the opposite, it continuously falls.
-            </p>
+            </span>
           }
         />
       </Col>
